e9b4a91c9a43f0237bd9bfe071ea195a973efc2b
John Murray <jmurrayufo@gmail.com>
2012-08-26 14:01:15 -0600

    Working name generation!

--- character.py
+++ character.py
@@ -1,71 +1,77 @@
 import random
-
+import csv
+import time
 
 class CharSheet():
     
-    def __init__(self):
-        self.name="DEFAULT"
-        self.ST=10
-        self.DX=10
-        self.IQ=10
-        self.HT=10
-        self.HP=10
-        self.WILL=10
-        self.PER=10
-        self.FP=10
-        self.Skills=dict()
+    def __init__(self,default=10):
+        self.name   = "DEFAULT"
+        self.ST     = default
+        self.DX     = default
+        self.IQ     = default
+        self.HT     = default
+        self.HP     = self.ST
+        self.WILL   = self.IQ
+        self.PER    = self.IQ
+        self.FP     = self.HT
+        self.Skills = dict()
 
         self.Height=0
         self.Weight=0
-        self.Points=0
+        self.Points=0+(default-10)*(10+20+20+10)
     
-    def roll(self,points):
+    def Roll(self,points=0):
         self.Height = random.gauss(1.778,0.15)
+
+        self.GenerateName()
+
+        statsSelection = ["ST","ST","DX","DX","IQ","IQ","HT","HT","HP","WILL","PER","FP"]
 
         while(self.Points < points):
             # Spend points on stuff!
-            selection = random.randint(1,8)
+            selection = random.choice(statsSelection)
 
-            if(selection == 1):
+            if(selection == "ST"):
                 self.ST+=1
                 self.HP+=1
                 self.Points+=10
 
-            elif(selection == 2):
+            elif(selection == "DX"):
                 self.DX+=1
                 self.Points+=20
 
-            elif(selection == 3):
+            elif(selection == "IQ"):
                 self.IQ+=1
                 self.WILL+=1
                 self.PER+=1
                 self.Points+=20
 
-            elif(selection == 4):
+            elif(selection == "HT"):
                 self.HT+=1
                 self.FP+=1
                 self.Points+=10
 
-            elif(selection == 5):
+            elif(selection == "HP" and self.HP+1 <= self.ST*1.3):
+                # Limited to +/- 30% of ST
                 self.HP+=1
                 self.Points+=2
 
-            elif(selection == 6):
+            elif(selection == "WILL" and self.WILL <=20):
                 self.WILL+=1
                 self.Points+=5
 
-            elif(selection == 7):
+            elif(selection == "PER" and self.PER <= 20):
                 self.PER+=1
                 self.Points+=5
 
-            elif(selection == 8):
+            elif(selection == "FP" and self.FP+1 <= self.HT*1.3):
                 self.FP+=1
                 self.Points+=3
 
     def __str__(self):
         return self.name
 
-    def printStats(self):
+    def PrintStats(self):
         print "Name:",self.name
         print " ST:",self.ST
         print " DX:",self.DX
@@ -77,10 +83,65 @@ class CharSheet():
         print " FP:",self.FP
         print " Points:",self.Points
 
-            
-x=CharSheet()
+    def GenerateName(self,gender="male"):
+        self.name = self.ParseNameFile('firstnames.csv',gender) + " " + self.ParseNameFile('lastnames.csv')
 
-print x
-x.printStats()
-x.roll(200)
-x.printStats()
+    def ParseNameFile(self,namesFile,gender="male"):
+        with open(namesFile,'r') as namesFile:
+            nameReader = csv.reader(namesFile)
+            firstNamesMale = list()
+            firstNamesFemale = list()
+
+            # Skip the header
+            nameReader.next()
+            for i in nameReader:
+                firstNamesMale.append(i[1:3])
+                if(gender=="female"):
+                    firstNamesFemale.append(i[3:5])
+
+        # Convert counts to ints    
+        if(gender=="male"):
+            for i in firstNamesMale:
+                i[1]=int(i[1])
+        elif(gender=="female"): 
+            for i in firstNamesFemale:
+                i[1]=int(i[1])
+
+        # Prep ints for random selection
+        if(gender=="male"):
+            runningTotalMale=0
+            for i in firstNamesMale:
+                i[1]=i[1]+runningTotalMale
+                runningTotalMale=i[1]
+        if(gender=="female"):
+            runningTotalFemale=0
+            for i in firstNamesFemale:
+                i[1]=i[1]+runningTotalFemale
+                runningTotalFemale=i[1]
+
+        if(gender=="male"):
+            selecton = random.randint(0,runningTotalMale)
+            for i in firstNamesMale:
+                if selecton <= i[1]:
+                    return i[0].title()
+
+        elif(gender=="female"):
+            selecton = random.randint(0,runningTotalFemale)
+            for i in firstNamesFemale:
+                if selecton <= i[1]:
+                    return i[0].title()
+
+
+x=CharSheet(8)
+
+x.Roll(0)
+
+x.PrintStats()
+
+
+            
+
+
+
+
+
