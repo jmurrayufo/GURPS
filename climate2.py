3cb175dd97ee685faaac10b9d4604c080059fcfa
John Murray <jmurrayufo@gmail.com>
2012-09-05 23:03:47 -0600

    Sync from work copy

--- climate2.py
+++ climate2.py
@@ -13,9 +13,9 @@ dateLandFall = datetime(2075,9,9)
 
 dateMissionEnd = datetime(2080,5,26)
 
-offsetDays = timedelta(3)
+offsetDays = timedelta(7)
 
-timeToday = timedelta(hours=11,minutes=55,seconds=00)
+timeToday = timedelta(hours=12,minutes=0,seconds=1)
 
 # Days for one cycle
 moon1Period = 17.974322903911
@@ -68,9 +68,6 @@ def GenTemperature(date,confFile):
     assert type(date) == type(datetime(1,1,1)),"Date must be of type Datetime. Is of type %s" %(type(date))
     assert type(confFile) == type(str()),"confFile must be of type str(). Is of type %s" %(type(confFile))
 
-    yearProgress = date-date.replace(month=1,day=1,hour=0,minute=0,second=0)
-    yearProgress = yearProgress.total_seconds()/31643136.0
-
     with open(confFile,"rb") as f:
         data = json.load(f)
     
@@ -80,25 +77,22 @@ def GenTemperature(date,confFile):
         x.append(i)
         y.append(data["Temperature"]["Map"][str(i)])
     interpFunc = interpolate.interp1d(x, y,'cubic')
-    # print "TempBase:",interpFunc(yearProgress*12+1)
-    temps = list()
 
-    random.seed(date.replace(minute=0,second=0))
-    temps.append(random.gauss(interpFunc(yearProgress*12+1),data["Temperature"]["Variance"]))
+    timeOfDayOffset = sin((date.hour+date.minute/60.+date.second/60./60.-6.)*2.*pi/24.)*data["Temperature"]["Variance"]
 
-    random.seed(date.replace(minute=0,second=0) + timedelta(hours=1))
-    temps.append(random.gauss(interpFunc(yearProgress*12+1),data["Temperature"]["Variance"]))
+    dayOffset=list()
 
-    hourProgress = date-date.replace(minute=0,second=0)
-    hourProgress = hourProgress.total_seconds() / (60.0*60.0)
+    dateToday = date.replace(hour=0,minute=0,second=0,microsecond=0)
 
-    # print "Date:",date
-    # print "Hour Prog:",hourProgress
-    # print "Temps:",temps
-    # print Cosine_Interpolate(temps[0],temps[1],hourProgress)
-    # print
-    return Cosine_Interpolate(temps[0],temps[1],hourProgress)
+    random.seed(YearProgress(dateToday) + DayProgress(dateToday))
+    dayOffset.append( random.gauss(0,data["Temperature"]["Variance"]) ) 
 
+    random.seed(YearProgress(dateToday + timedelta(days=1)) + DayProgress(dateToday))
+    dayOffset.append( random.gauss(0,data["Temperature"]["Variance"]) )
+
+    dayOffset = Cosine_Interpolate(dayOffset[0],dayOffset[1],DayProgress(date))
+
+    return float(interpFunc(YearProgress(date)*12+1)) + timeOfDayOffset + dayOffset
 
 
 def Cosine_Interpolate(a, b, x):
@@ -106,6 +100,15 @@ def Cosine_Interpolate(a, b, x):
     f = (1 - cos(ft)) * 0.5
     return  a*(1-f) + b*f
 
+def DayProgress(date):
+    return (date - date.replace(hour=0,minute=0,second=0)).total_seconds()/(24.*60.*60.)
+
+def YearProgress(date):
+    return (date-date.replace(month=1,day=1,hour=0,minute=0,second=0)).total_seconds()/31643136.0
+
+def LunarInfo(date,period):
+    offset = (date-dateLandFall).total_seconds()/(24.*60.*60.)
+    return (sin(2*pi*offset/period)+1)/2.
 
 ### Output ###
 
@@ -118,16 +121,17 @@ def Cosine_Interpolate(a, b, x):
 # print "Moon 1 Phase",sin(2*pi*missionElapsed/(moon1Period * 24 * 60 * 60))
 
 # print "Moon 1 Phase",sin(2*pi*missionElapsed/(moon2Period * 24 * 60 * 60))
-step = timedelta(minutes=5)
+print GenTemperature(datetime(2075,12,31,12,59,59),"data/weatherHome.json")
+print "Lunar Intensity: %.02f%%" % (LunarInfo(timeToday,moon1Period)*100)
+print "Lunar Intensity: %.02f%%" % (LunarInfo(timeToday,moon2Period)*100)
+
 x=list()
-y=list()
-for i in range(365*10):
+y1=list()
+y2=list()
+for i in np.linspace(0,365*5,24*365):
     x.append(i)
-    timeToday += step
-    y.append(GenTemperature(timeToday,"data/weatherHome.json"))
-    # time.sleep(1)
-    
-plt.plot(x,y)
+    y1.append(LunarInfo(timeToday+timedelta(days=i),moon1Period)*100)
+    y2.append(LunarInfo(timeToday+timedelta(days=i),moon2Period)*100)
+
+plt.plot(x,y1,x,y2)
 plt.show()
-GenTemperature(timeToday,"data/weatherHome.json")
-quit()
