7aeb6808c5acad66b10d62bda68ec56e04fbd4ae
John Murray <jmurrayufo@gmail.com>
2013-03-17 12:54:23 -0600

    Misc old changes being commited for sync

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
-import matplotlib.backends.backend_pdf as pdf
-import matplotlib.pyplot as plt
+import calendar
 
-pp = pdf.PdfPages("test.pdf")
-
-x=list()
-y=list()
-for i in range(100):
-    x.append(i)
-    y.append(i**2)
-
-plt.plot(x,y)
-plt.savefig(pp, format='pdf')
-
-plt.figure(2)
-plt.clf()
-plt.text(0.5,0,"Hello World!")
-plt.savefig(pp, format='pdf')
-
-
-pp.close()
+print calendar.month(2275,9)
+print calendar.month(2275,10)
--- travelTime.py
+++ travelTime.py
@@ -1,10 +1,12 @@
 from math import *
+import time
+import datetime
 
 # G's of Acceleration
-Accel = 0.1
+Accel = 0.2
 
-# Measured in light years
-Distance = 1
+# % Speed of light to coast at
+V_Coast = 0.25
 
 # Conversion Factor
 metersInALightYear = 9.461e15
@@ -37,26 +39,40 @@ def LorenzFactor(vel):
 ### RUN ###
 ###########
 
-Distance_Total = Distance * metersInALightYear
 dDistance = 0
 dTime = 0
 V = Vi
+V_Coast *= 299792458.0
+update = time.time()
 
-while(dDistance <= Distance_Total/2):
+while(V <= V_Coast):
     # Calculate Acceleration
     V_Old = V
     V += Accel * timeStep * LorenzFactor(V)
-    # If Speed of light reached...
-    if( V >= 299792458):
-        print "Illegal V Reached!"
-        print "Vel Change:",Accel * timeStep * LorenzFactor(V_Old)
-        print "Vel Change(Newton):",Accel * timeStep
-        V = 299792458
+    while(V >= 299792458.0):
+        print "Reduce Time Steps!"
+        print "New Time Step:",timeStep
+        print "    V:",V
+        print "V_Old:",V_Old
+        time.sleep(0.1)
+        timeStep/=2.0
+        V=V_Old
+        V += Accel * timeStep * LorenzFactor(V)
+
     # Calculate Disance Traveled
-    dDistance += V * LorenzFactor(V)
+    dDistance += V * timeStep * LorenzFactor(V)
     dTime += timeStep * LorenzFactor(V)
 
-print "Distance Traveled:",dDistance
-print "Time Taken(h):",dTime/60./60.
-print "Final Velocity(m/s):",V
+    if(time.time() > update + 10):
+        print "dDistance:",dDistance
+        print "dTime:",dTime
+        print "%% of trip:",dDistance/float(Distance_Total)*100
+        update = time.time()
 
+print "Distance Traveled(m):",dDistance
+print "Distance Traveled(ly):",dDistance/9.461e15
+print "Time Taken:",datetime.timedelta(seconds = dTime)
+print "Time Taken(y): %0.1f" % (datetime.timedelta(seconds = dTime).days/365.24)
+print "Final Velocity(m/s):",V
+print "Final LorenzFactor:",LorenzFactor(V)
+print "%% C: %0.2f%%" % (V/299792458.0*100)
