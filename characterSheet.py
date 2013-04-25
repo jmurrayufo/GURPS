dceafd8e304f993a73b3bbcd1953186446061d8d
John Murray <jmurrayufo@gmail.com>
2013-04-25 10:44:46 -0600

    Cleanup, new funcs

--- characterSheet.py
+++ characterSheet.py
@@ -4,6 +4,7 @@ import json
 import operator
 import random
 import math
+import types
 
 """
 GURPS is a trademark of Steve Jackson Games, and its rules and art are copyrighted 
@@ -139,6 +140,8 @@ class CharSheet( ):
 
       self.CalcUpdatePointsTotals( )
 
+      # Menus
+
 
    def __str__(self):
       return self.Name
@@ -148,7 +151,7 @@ class CharSheet( ):
       return {"name":self.Name}
 
 
-   def AddSkill( self, csvLine, points=0 ):
+   def AddSkillCSV( self, csvLine, points=0 ):
       """
       Attempt to add a skill from the CSV format
       """
@@ -157,10 +160,17 @@ class CharSheet( ):
       self.Skills.append( Skill( csvLine, points ) )
 
 
+   def AddSkill( self, newSkill ):
+      if( self.HasSkill( newSkill.Name ) ):
+         return
+      self.Skills.append( newSkill )
+
+
    def HasSkill( self, skillName ):
       """
       Check if the named skill is the in skill list
       """
+      assert( type( skillName ) == str )
       for i in self.Skills:
          if( i.Name == skillName ):
             return True
@@ -174,6 +184,8 @@ class CharSheet( ):
          return int( math.floor( points / div ) )    
       """
       Return the true value of a given attribute.
+         attribute: String of attribute to be returned. Can be..
+            ST, DX, IQ, HT, HP, WILL, PER, FP
       """
       # Modify input to uppercase
       attribute = attribute.upper()
@@ -198,14 +210,14 @@ class CharSheet( ):
 
    def Print(self):
       print "Name:",self.Name
-      print "     ST: %3d [%3d]" % ( self.GetAttrValue( 'ST'), self.STp )
-      print "     DX: %3d [%3d]" % ( self.GetAttrValue( 'DX' ), self.DXp )
-      print "     IQ: %3d [%3d]" % ( self.GetAttrValue( 'IQ' ), self.IQp )
-      print "     HT: %3d [%3d]" % ( self.GetAttrValue( 'HT' ), self.HTp )
-      print "     HP: %3d [%3d]" % ( self.GetAttrValue( 'HP' ), self.HPp )
-      print "   WILL: %3d [%3d]" % ( self.GetAttrValue( 'WILL' ), self.WILLp )
-      print "    PER: %3d [%3d]" % ( self.GetAttrValue( 'PER' ), self.PERp )
-      print "     FP: %3d [%3d]" % ( self.GetAttrValue( 'FP' ), self.FPp )
+      print "     ST: %2d [%3d]" % ( self.GetAttrValue( 'ST'), self.STp )
+      print "     DX: %2d [%3d]" % ( self.GetAttrValue( 'DX' ), self.DXp )
+      print "     IQ: %2d [%3d]" % ( self.GetAttrValue( 'IQ' ), self.IQp )
+      print "     HT: %2d [%3d]" % ( self.GetAttrValue( 'HT' ), self.HTp )
+      print "     HP: %2d [%3d]" % ( self.GetAttrValue( 'HP' ), self.HPp )
+      print "   WILL: %2d [%3d]" % ( self.GetAttrValue( 'WILL' ), self.WILLp )
+      print "    PER: %2d [%3d]" % ( self.GetAttrValue( 'PER' ), self.PERp )
+      print "     FP: %2d [%3d]" % ( self.GetAttrValue( 'FP' ), self.FPp )
       if( len( self.Skills ) ):
          print " Skills:"      
          sortedSkills = sorted(self.Skills, key=lambda x: x.Name)
@@ -213,6 +225,114 @@ class CharSheet( ):
             print " ",i.Print( self )
       print "   Total Points: %3d"%( self.PointsTotal )
       print " Unspent Points: %3d"%( self.PointsUnspent )
+
+
+   def Main( self ):
+      """
+      Main loop of application if used as such
+      """
+      mainMenu = (
+         ( "Attributes", self.PormptAttributes ),
+         ( "Skills", self.PromptSkills ),
+         ( "Save", None ),
+         (" Load", None ),
+         ( "Quit", None )
+         )
+
+      while True:
+         self.Print()
+         for idx,val in enumerate( mainMenu ):
+            print "[%d] %s"%( idx + 1, val[0] )
+            # print val[0]
+
+         try:
+            selection = input(">")
+         except ( NameError, SyntaxError ):
+            continue
+
+         try:
+            # Map selection back to true index
+            selection = mainMenu[selection - 1][1]
+         except IndexError:
+            continue
+
+         if( type( selection ) == types.FunctionType 
+            or type( selection ) == types.InstanceType 
+            or type( selection ) == types.MethodType
+            ):
+            selection()
+            continue
+         if( selection == None ):
+            break
+
+   def PormptAttributes( self ):
+      attrMenu = (
+         ( "ST",   "STp",   "Strength"       ),
+         ( "DX",   "DXp",   "Dexterity"      ),
+         ( "IQ",   "IQp",   "Intelligence"   ),
+         ( "HT",   "HTp",   "Health"         ),
+         ( "HP",   "HPp",   "Hit Points"     ),
+         ( "Will", "Willp", "Will"           ),
+         ( "PER",  "PERp",  "Perception"     ),
+         ( "FP",   "FPp",   "Fatigue Points" )
+         )
+
+      while True:
+         for idx,val in enumerate( attrMenu ):
+            print "[%d] %s"%( idx + 1, val[2] )
+
+         try:
+            # Map selection back to true index
+            selection = input(">") - 1
+         except (NameError):
+            continue
+         except (SyntaxError):
+            break
+
+         try:
+            selection = attrMenu[selection]
+         except IndexError:
+            continue
+
+         # We have a valid entry, print out the current value and then modify the points
+         print "Selected %s %d [%d]"%(
+            selection[0], 
+            self.GetAttrValue( selection[0] ),
+            getattr( self, selection[1])
+             )
+
+         print "Enter the change in points..."
+         dPoints = input(">")
+         setattr( self, selection[1], getattr( self, selection[1] ) + dPoints )
+         self.PointsUnspent -= dPoints
+         break
+
+
+   def PromptSkills( self ):
+      skillMenu = (
+         ( "Add Skill From HDD", None ),
+         ( "Add Skill Manually", None ),
+         ( "Find Default",       None )
+         ( "Remove Skill",       None )
+         ) 
+
+      while True:
+         for idx,val in enumerate( skillMenu ):
+            print "[%d] %s"%( idx + 1, val[0] )
+
+         try:
+            # Map selection back to true index
+            selection = input(">") - 1
+         except (NameError):
+            continue
+         except (SyntaxError):
+            break
+
+         try:
+            selection = skillMenu[selection]
+         except IndexError:
+            continue
+         break
 
 
    def CalcUpdatePointsTotals( self ):
@@ -251,16 +371,17 @@ if __name__ == '__main__':
    john = CharSheet()
    john.Name = 'John'
 
-   with open("data/gameref/skills.csv",'r') as fp:
-       skillreader = csv.reader( fp, delimiter=',')
-       # Gobble header
-       header = skillreader.next()
-       for idx,val in enumerate( skillreader ):
-           if( len( val ) == 5):
-               john.AddSkill( val )
-           else:
-               pass
+   # with open("data/gameref/skills.csv",'r') as fp:
+   #     skillreader = csv.reader( fp, delimiter=',')
+   #     # Gobble header
+   #     header = skillreader.next()
+   #     for idx,val in enumerate( skillreader ):
+   #         if( len( val ) == 5):
+   #             john.AddSkill( val )
+   #         else:
+   #             pass
 
    # print john
 
-   john.Print()
+
+   john.Main()
